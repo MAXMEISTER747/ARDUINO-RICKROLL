5b60c537d6f4c3d4e601ce0aeac214323fe6a088
MAXMEISTER747 <84752597+MAXMEISTER747@users.noreply.github.com>
2021-11-17 18:53:10 +1100

    General update
    Updated almost everything to have better funtionality
    Added the mac-cmd rickroll; a better way to rickroll people on mac

--- rickroll/mac/mac.ino
+++ rickroll/mac/mac.ino
@@ -1,21 +1,17 @@
 #include <Keyboard.h>
-char ctrlKey = KEY_LEFT_GUI ;
 
 void setup(){
   Keyboard.begin();
   delay (1000);
-  Keyboard.press(ctrlKey);
-  Keyboard.press('n');
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('t');
   delay(10);
   Keyboard.releaseAll();
   Keyboard.print("https://www.youtube.com./watch?v=dQw4w9WgXcQ");
   Keyboard.press(KEY_RETURN);
   delay(10);
   Keyboard.releaseAll();
-  delay(3000);
-  Keyboard.press(' ');
-  delay(10);
-  Keyboard.releaseAll(); 
+ 
 
 Keyboard.end();
 }
